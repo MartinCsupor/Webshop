85de986009decb2e29a317789b855a28635ea038
MartinCsupor <csupormartin@turr.hu>
2026-05-28 09:19:21 +0200

    Add project files.

---
+++ Models/Product.cs
@@ -0,0 +1,33 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Webshop.Models
+{
+    internal class Product
+    {
+        [AutoIncrement, PrimaryKey]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int StockQuantity { get; set; }
+
+        public string Category { get; set; }
+
+        public string Brand { get; set; }
+
+        public double Weight { get; set; }
+
+        public bool IsAvailable { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,45 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Webshop.Models
+{
+    internal class User
+    {
+        [AutoIncrement, PrimaryKey]
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+
+        public string PasswordHash { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public string Role { get; set; }
+
+        public User() { }
+        public User(int id, string username, string email, string passwordHash, string firstName, string lastName, DateTime createdAt, bool isActive, string role)
+        {
+            Id = id;
+            Username = username;
+            Email = email;
+            PasswordHash = passwordHash;
+            FirstName = firstName;
+            LastName = lastName;
+            CreatedAt = createdAt;
+            IsActive = isActive;
+            Role = role;
+        }
+    }
+}
