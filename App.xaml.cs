58dba5d7799ebeaae3408c3537570aa8079d824e
MartinCsupor <csupormartin@turr.hu>
2026-05-28 09:39:18 +0200

    Models, Services

--- App.xaml.cs
+++ App.xaml.cs
@@ -9,6 +9,8 @@ namespace Webshop
     /// </summary>
     public partial class App : Application
     {
+        static string database = "Webshop.db";
+        static string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        public static string databasePath = System.IO.Path.Combine(path, database);
     }
-
 }
